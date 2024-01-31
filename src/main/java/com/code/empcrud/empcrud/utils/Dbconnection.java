bb60afffd5f60efa75c5f4f555e7e50c7dda4064
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2024-01-31 19:32:25 +0200

    database connection

--- src/main/java/com/code/empcrud/empcrud/utils/Dbconnection.java
+++ src/main/java/com/code/empcrud/empcrud/utils/Dbconnection.java
@@ -6,15 +6,17 @@ import java.sql.SQLException;
 
 public class Dbconnection {
 
-    private static String jdbcUrl;
-    private static String jdbcUsername;
-    private static String jdbcPassword;
+    private static String jdbcUrl = "jdbc:postgresql://localhost:5432/webapp";
+    private static String jdbcUsername = "postgres";
+    private static String jdbcPassword = "P0S1tiv@!";
 
     public Connection connectToDb() throws SQLException {
         Connection conn = null;
         try{
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+            if(conn != null)
+                System.out.println("We are connected to the database successfully");
         } catch(SQLException ex){
             throw new RuntimeException("SQLException");
         } catch(ClassNotFoundException ex){
